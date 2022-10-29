74cabaa8780ce503bebea3aea3dc3d773db35a6f
Aditya Ranjan <116148494+A9DITYA@users.noreply.github.com>
2022-10-29 23:53:50 +0530

    Add files via upload

---
+++ WAP(SUM).cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter number");
+	
+	scanf("%d",&a);
+	
+	printf("Enter number");
+	
+	scanf("%d",&b);
+	
+	printf("sum of two number is %d" , a+b);
+	
+	return 0;
+	
+	
+	
+}
